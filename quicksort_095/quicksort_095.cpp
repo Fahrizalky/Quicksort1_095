c29e005eee811c76e53cb86877e1778c4b6c6c00
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2023-03-31 15:45:01 +0700

    Add project files.

---
+++ quicksort_095/quicksort_095.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+//array of integers to hold values
+int arr[20];
+int cmp_count = 0; // number of comprasion
+int mov_count = 0; // number of data movemnts
+int n;
+
+void input() {
+	while (true)
+	{
+		cout << "Masukkan panajng element array: ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum panjang array adalah 20" << endl;
+	}
